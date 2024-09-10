a3b8c0d2f0b8cb142a3cb7309a1a0c2fa1865eb4
Frederik Bastrup <s235117@dtu.dk>
2024-09-10 11:06:43 +0200

    added a test

--- test.sh
+++ test.sh
@@ -17,3 +17,7 @@ out3="5;"
 
 [[ $(./cmd_int <<< "$in3") == "$out3"* ]] && echo "PASSED" || echo "FAILED"
 
+in4="aabbaaaaaabcbbbbbababbcbaaabbbaaa"
+out4="0,1,4,5,6,7,8,17,24,25,26,30,31,32;"
+
+[[ $(./cmd_int <<< "$in4") == "$out4"* ]] && echo "PASSED" || echo "FAILED"
