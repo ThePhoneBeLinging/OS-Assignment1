9f7c42dcf549ec8b6bfcae99caf9e4165e79c19f
eSavino <s235096@dtu.dk>
2024-09-10 10:59:10 +0200

    added more tests to test

--- test.sh
+++ test.sh
@@ -7,4 +7,13 @@ out="0,3,5;"
 
 [[ $(./cmd_int <<< "$in") == "$out"* ]] && echo "PASSED" || echo "FAILED"
 
+in2="abbbaccaabbbbac"
+out2="7,8;"
+
+[[ $(./cmd_int <<< "$in2") == "$out2"* ]] && echo "PASSED" || echo "FAILED"
+
+in3="abccbaabcq"
+out3="5;"
+
+[[ $(./cmd_int <<< "$in3") == "$out3"* ]] && echo "PASSED" || echo "FAILED"
 
