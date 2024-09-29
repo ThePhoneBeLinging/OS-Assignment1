85b58ff6b4a6d264a69a6ccb3ce4c5036ff7e7aa
Frederik Bastrup <s235117@dtu.dk>
2024-09-29 13:34:37 +0200

    fixed macros and started working on simple_init and simple_malloc

--- 2Assignment/mm.c
+++ 2Assignment/mm.c
@@ -11,26 +11,25 @@
 #include "mm.h"
 
 
-
 /* Proposed data structure elements */
 
 typedef struct header {
-  struct header * next;     // Bit 0 is used to indicate free block 
-  uint64_t user_block[0];   // Standard trick: Empty array to make sure start of user block is aligned
+    struct header *next;     // Bit 0 is used to indicate free block
+    uint64_t user_block[0];   // Standard trick: Empty array to make sure start of user block is aligned
 } BlockHeader;
 
 /* Macros to handle the free flag at bit 0 of the next pointer of header pointed at by p */
-#define GET_NEXT(p)    (void *) (p->next)    /* TODO: Mask out free flag */
-#define SET_NEXT(p,n)  p->next = (void *) n  /* TODO: Preserve free flag */
+#define GET_NEXT(p)    (void *) (((uintptr_t) p->next) & ~1)    /* MAYBE DONE TODO: Mask out free flag */
+#define SET_NEXT(p, n)  p->next = (void *)(((uintptr_t)(n) & ~1) | ((uintptr_t)GET_FREE(p))) /* TODO: Preserve free flag */
 #define GET_FREE(p)    (uint8_t) ( (uintptr_t) (p->next) & 0x1 )   /* OK -- do not change */
-#define SET_FREE(p,f)  /* TODO: Set free bit of p->next to f */
-#define SIZE(p)        (size_t) ( 0 ) /* TODO: Caluculate size of block from p and p->next */ 
+#define SET_FREE(p, f)  p->next = (void *)(((uintptr_t)GET_NEXT(p)) | ((uintptr_t)f)) /* MAYBE DONE TODO: Set free bit of p->next to f */
+#define SIZE(p)        (size_t) ((uintptr_t)GET_NEXT(p) - (uintptr_t)p) /* MAYBE DONE TODO: Calc size of block from p and p->next */
 
 #define MIN_SIZE     (8)   // A block should have at least 8 bytes available for the user
 
 
-static BlockHeader * first = NULL;
-static BlockHeader * current = NULL;
+static BlockHeader *first = NULL;
+static BlockHeader *current = NULL;
 
 /**
  * @name    simple_init
@@ -38,18 +37,22 @@ static BlockHeader * current = NULL;
  *
  */
 void simple_init() {
-  uintptr_t aligned_memory_start = memory_start;  /* TODO: Alignment */
-  uintptr_t aligned_memory_end   = memory_end;    /* TODO: Alignment */
-  BlockHeader * last;
+    uintptr_t aligned_memory_start = memory_start + (memory_start % 64);
+    uintptr_t aligned_memory_end = memory_end - (memory_end % 64);
+    BlockHeader *last;
 
-  /* Already initalized ? */
-  if (first == NULL) {
-    /* Check that we have room for at least one free block and an end header */
-    if (aligned_memory_start + 2*sizeof(BlockHeader) + MIN_SIZE <= aligned_memory_end) {
-      /* TODO: Place first and last blocks and set links and free flags properly */
+    /* Already initalized ? */
+    if (first == NULL) {
+        /* Check that we have room for at least one free block and an end header */
+        if (aligned_memory_start + 2 * sizeof(BlockHeader) + MIN_SIZE <= aligned_memory_end) {
+            /* TODO: Place first and last blocks and set links and free flags properly */
+            first = (BlockHeader *) aligned_memory_start;
+            last = (BlockHeader *) aligned_memory_end;
+            SET_FREE(first, 0);
+            SET_NEXT(first, last);
+            SET_FREE(last, 1);
+        }
     }
-    current = first;     
-  } 
 }
 
 
@@ -63,41 +66,39 @@ void simple_init() {
  * @retval Pointer to the start of the allocated memory or NULL if not possible.
  *
  */
-void* simple_malloc(size_t size) {
-  
-  if (first == NULL) {
-    simple_init();
-    if (first == NULL) return NULL;
-  }
-
-  size_t aligned_size = size;  /* TODO: Alignment */
-
-  /* Search for a free block */
-  BlockHeader * search_start = current;
-  do {
- 
-    if (GET_FREE(current)) {
-
-      /* Possibly coalesce consecutive free blocks here */
-
-      /* Check if free block is large enough */
-      if (SIZE(current) >= aligned_size) {
-        /* Will the remainder be large enough for a new block? */
-        if (SIZE(current) - aligned_size < sizeof(BlockHeader) + MIN_SIZE) {
-          /* TODO: Use block as is, marking it non-free*/
-        } else {
-          /* TODO: Carve aligned_size from block and allocate new free block for the rest */
-        }
-        
-        return (void *) NULL; /* TODO: Return address of current's user_block and advance current */
-      }
+void *simple_malloc(size_t size) {
+    if (first == NULL) {
+        simple_init();
+        if (first == NULL) return NULL;
     }
 
-    current = GET_NEXT(current);
-  } while (current != search_start);
+    size_t aligned_size = size + (size % 8);
 
- /* None found */
-  return NULL;
+    /* Search for a free block */
+    BlockHeader *search_start = current;
+    do {
+        if (GET_FREE(current)) {
+
+            /* Possibly coalesce consecutive free blocks here */
+
+            /* Check if free block is large enough */
+            if (SIZE(current) >= aligned_size) {
+                /* Will the remainder be large enough for a new block? */
+                if (SIZE(current) - aligned_size < sizeof(BlockHeader) + MIN_SIZE) {
+                    /* TODO: Use block as is, marking it non-free*/
+                } else {
+                    /* TODO: Carve aligned_size from block and allocate new free block for the rest */
+                }
+
+                return (void *) NULL; /* TODO: Return address of current's user_block and advance current */
+            }
+        }
+
+        current = GET_NEXT(current);
+    } while (current != search_start);
+
+    /* None found */
+    return NULL;
 }
 
 
@@ -110,16 +111,16 @@ void* simple_malloc(size_t size) {
  * @param void *ptr Pointer to the memory to free.
  *
  */
-void simple_free(void * ptr) {
-  BlockHeader * block = NULL; /* TODO: Find block corresponding to ptr */
-  if (GET_FREE(block)) {
-    /* Block is not in use -- probably an error */
-    return;
-  }
+void simple_free(void *ptr) {
+    BlockHeader *block = NULL; /* TODO: Find block corresponding to ptr */
+    if (GET_FREE(block)) {
+        /* Block is not in use -- probably an error */
+        return;
+    }
 
-  /* TODO: Free block */
+    /* TODO: Free block */
 
-  /* Possibly coalesce consecutive free blocks here */
+    /* Possibly coalesce consecutive free blocks here */
 }
 
 
